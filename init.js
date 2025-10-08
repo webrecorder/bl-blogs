1fc0109af0981eda6bbd9178e6a1987a460c1f5a
Ilya Kreymer <ikreymer@users.noreply.github.com>
2025-10-07 23:45:01 -0700

    Update init.js

--- init.js
+++ init.js
@@ -1,2 +1,2 @@
 // Fill in WACZ / Multi WACZ JSON and origin here
-// init("https://app.browsertrix.com/api/orgs/4349ac8e-7881-404a-ae0d-c3683b1ada78/collections/c3bfbcfb-13b6-43d4-a134-cb4e376ab65f/public/replay.json", "https://britishlibrary.typepad.co.uk", "");
+init("https://app.browsertrix.com/api/orgs/4349ac8e-7881-404a-ae0d-c3683b1ada78/collections/c3bfbcfb-13b6-43d4-a134-cb4e376ab65f/public/replay.json", "https://britishlibrary.typepad.co.uk", "");
