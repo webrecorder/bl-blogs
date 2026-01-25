46554df8023d97d38e26672b2703a40294039f82
Ilya Kreymer <ikreymer@gmail.com>
2026-01-24 16:40:24 -0800

    update to latest sw.js, use fixed wacz

--- init.js
+++ init.js
@@ -1,2 +1,4 @@
 // Fill in WACZ / Multi WACZ JSON and origin here
-init("https://app.browsertrix.com/api/orgs/4349ac8e-7881-404a-ae0d-c3683b1ada78/collections/c3bfbcfb-13b6-43d4-a134-cb4e376ab65f/public/replay.json", "https://britishlibrary.typepad.co.uk", "");
+//init("https://app.browsertrix.com/api/orgs/4349ac8e-7881-404a-ae0d-c3683b1ada78/collections/c3bfbcfb-13b6-43d4-a134-cb4e376ab65f/public/replay.json", "https://britishlibrary.typepad.co.uk", "");
+init("https://sfo2.digitaloceanspaces.com/dh-preserve/webarchives/bl/blblogs.wacz", "https://britishlibrary.typepad.co.uk", "");
+
